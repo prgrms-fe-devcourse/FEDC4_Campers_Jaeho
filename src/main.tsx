9a1b2adc4b25446cd46a4bdf037bfa0825acbc86
sukvvon <sukvvon@gmail.com>
2023-09-09 13:55:22 +0900

    feat: main.tsx에 ChakraProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
