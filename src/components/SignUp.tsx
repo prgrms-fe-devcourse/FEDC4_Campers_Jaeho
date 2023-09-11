a735e07b98499ccf1f29ddc0b1afaf54362732e4
sukvvon <sukvvon@gmail.com>
2023-09-11 15:39:16 +0900

    refactor: passwordValidation 생성 후 SignUp에 적용

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -12,6 +12,7 @@ import {
   Text,
 } from '@chakra-ui/react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import passwordValidation from '../utils/passwordValidation';
 
 interface SignUpFormValues {
   email: string;
@@ -77,19 +78,7 @@ const SignUp = () => {
                 value: 8,
                 message: '비밀번호는 8자 이상입니다',
               },
-              validate: {
-                hasNumber: (v: string) =>
-                  /\d+/g.test(v) || '비밀번호에 숫자 1개 이상은 필수입니다',
-                hasUppercase: (v: string) =>
-                  /[A-Z]+/g.test(v) ||
-                  '비밀번호에 영어 대문자 1개 이상은 필수입니다',
-                hasLowercase: (v: string) =>
-                  /[a-z]+/g.test(v) ||
-                  '비밀번호에 영어 소문자 1개 이상은 필수입니다',
-                hasSpecialCharacter: (v: string) =>
-                  /[!@#$%^&*()]+/g.test(v) ||
-                  '비밀번호에 특수문자 1개 이상은 필수입니다',
-              },
+              validate: passwordValidation,
             })}
           />
           <InputRightElement onClick={handlePasswordShow}>
