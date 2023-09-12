b4954fd89302c40c561b8361fef36e5c50d94cdd
nsr1349 <nsr1349@naver.com>
2023-09-12 16:25:30 +0900

    feat: useObserver 구현

---
+++ src/hooks/useObserver.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react';
+
+const useObserver = (showEvent = () => console.log('show!')) => {
+  const ref = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const el = ref.current;
+    if (!el) return;
+
+    const observer = new IntersectionObserver((e) => {
+      if (e[0].isIntersecting) showEvent();
+    });
+    observer.observe(el);
+    return () => observer.disconnect();
+  }, [ref]);
+
+  return ref;
+};
+
+export default useObserver;
