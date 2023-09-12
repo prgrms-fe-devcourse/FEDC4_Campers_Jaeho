a18564de666de12384a7b43708aabace8dddb96d
nsr1349 <nsr1349@naver.com>
2023-09-12 16:31:55 +0900

    feat: Main 페이지 생성 및 헤더 구현

---
+++ src/pages/Main.tsx
@@ -0,0 +1,29 @@
+import { SearchIcon } from '@chakra-ui/icons';
+import { Flex, Text } from '@chakra-ui/react';
+// import MainPageContent from '../components/MainPageContent';
+// import BottomNavBar from '../components/BottomNavbar';
+
+function Main() {
+  return (
+    <>
+      <Flex height="60px" padding="0 15px" alignItems="center">
+        <Text fontSize="24px" flexGrow="1">
+          Campers
+        </Text>
+        {/* 검색페이지로 라우트 */}
+        <SearchIcon
+          boxSize={8}
+          cursor="pointer"
+          padding="5px"
+          borderRadius="15px"
+        />
+      </Flex>
+
+      {/* <MainPageContent />
+
+      <BottomNavBar /> */}
+    </>
+  );
+}
+
+export default Main;
