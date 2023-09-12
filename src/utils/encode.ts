c088489950aca97e5208529a4df741218b4fb77a
이범진 <ibeomjin@ibeomjins-MacBook-Pro.local>
2023-09-12 14:47:09 +0900

    feat: encode util 함수 추가

---
+++ src/utils/encode.ts
@@ -0,0 +1,25 @@
+export default async function convertImagesToBase64(
+  images: File[]
+): Promise<string[]> {
+  const base64Images: string[] = await Promise.all(
+    images.map(async (imageFile) => {
+      return new Promise<string>((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          if (event.target && typeof event.target.result === 'string') {
+            resolve(event.target.result);
+          } else {
+            reject(new Error('Failed to convert image to base64.'));
+          }
+        };
+
+        reader.onerror = (error) => {
+          reject(error);
+        };
+
+        reader.readAsDataURL(imageFile);
+      });
+    })
+  );
+  return base64Images;
+}
