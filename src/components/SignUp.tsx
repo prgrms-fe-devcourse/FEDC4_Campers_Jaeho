5ad1c1405355a48c4a61c995cdb06318ad5168ac
sukvvon <sukvvon@gmail.com>
2023-09-12 17:07:26 +0900

    refactor: SignUp의 Input의 register들을 registers로 묶음

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -40,6 +40,36 @@ const SignUp = () => {
     reset();
   };
 
+  const registers = {
+    signUpEmail: register('signUpEmail', {
+      required: '이메일 입력을 필수입니다',
+    }),
+    signUpPassword: register('signUpPassword', {
+      required: '비밀번호는 8자 이상입니다',
+      minLength: {
+        value: 8,
+        message: '비밀번호는 8자 이상입니다',
+      },
+      validate: passwordValidation,
+    }),
+    signUpPasswordConfilm: register('signUpPasswordConfilm', {
+      required: '재확인 비밀번호를 입력해주세요',
+      validate: (
+        _: string,
+        { signUpPassword, signUpPasswordConfilm }: SignUpFormValues
+      ) =>
+        signUpPassword === signUpPasswordConfilm ||
+        '비밀번호가 일치하지 않습니다',
+    }),
+    signUpNickname: register('signUpNickname', {
+      required: '',
+      minLength: {
+        value: 2,
+        message: '닉네임은 2글자 이상입니다',
+      },
+    }),
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl isRequired isInvalid={errors.signUpEmail && true}>
@@ -49,9 +79,7 @@ const SignUp = () => {
             id="signup-email"
             type="email"
             placeholder="이메일을 형식에 맞게 입력해주세요"
-            {...register('signUpEmail', {
-              required: '이메일 입력은 필수입니다',
-            })}
+            {...registers.signUpEmail}
           />
         </InputGroup>
         <FormErrorMessage>
@@ -67,14 +95,7 @@ const SignUp = () => {
             id="signup-password"
             type={isShow ? 'text' : 'password'}
             placeholder="비밀번호를 형식에 맞게 입력해주세요"
-            {...register('signUpPassword', {
-              required: '비밀번호는 8자 이상입니다',
-              minLength: {
-                value: 8,
-                message: '비밀번호는 8자 이상입니다',
-              },
-              validate: passwordValidation,
-            })}
+            {...registers.signUpPassword}
           />
           <InputRightElement onClick={handlePasswordShow}>
             {isShow ? <ViewOffIcon /> : <ViewIcon />}
@@ -97,15 +118,7 @@ const SignUp = () => {
             id="singup-password-confilm"
             type={isShow ? 'text' : 'password'}
             placeholder="재확인 비밀번호를 입력해주세요"
-            {...register('signUpPasswordConfilm', {
-              required: '재확인 비밀번호를 입력해주세요',
-              validate: (
-                _: string,
-                { signUpPassword, signUpPasswordConfilm }: SignUpFormValues
-              ) =>
-                signUpPassword === signUpPasswordConfilm ||
-                '비밀번호가 일치하지 않습니다',
-            })}
+            {...registers.signUpPasswordConfilm}
           />
           <InputRightElement onClick={handlePasswordShow}>
             {isShow ? <ViewOffIcon /> : <ViewIcon />}
@@ -123,13 +136,7 @@ const SignUp = () => {
           id="signup-nickname"
           type="text"
           placeholder="닉네임은 2글자 이상입니다"
-          {...register('signUpNickname', {
-            required: '',
-            minLength: {
-              value: 2,
-              message: '닉네임은 2글자 이상입니다',
-            },
-          })}
+          {...registers.signUpNickname}
         />
         <FormErrorMessage>
           {errors.signUpNickname && errors.signUpNickname.message}
