3a4e75dcce5e66e0767478ac12a44462cc11c1d1
kim <perfumeplaylist.gmail.com>
2023-09-14 14:41:34 +0900

    fix: Avatar 컴포넌트로 수정

--- src/components/common/AvatarImage.tsx
+++ src/components/common/AvatarImage.tsx
@@ -1,46 +1,35 @@
-import { ImageProps, Image, Box } from '@chakra-ui/react';
+import { AvatarProps, Avatar, AvatarBadge } from '@chakra-ui/react';
 import { useNavigate } from 'react-router-dom';
 
-export type AvatarImageProrps = ImageProps & {
-  width: string;
+export type AvatarImageProps = AvatarProps & {
+  size: string;
   userId: string;
   src?: string;
   isOnline: boolean;
 };
 
 const AvatarImage = ({
-  width,
+  size,
   isOnline,
   userId,
   src,
   ...props
-}: AvatarImageProrps) => {
+}: AvatarImageProps) => {
   const navigate = useNavigate();
 
   return (
     <>
-      <Image
+      <Avatar
         {...props}
         onClick={() => navigate(`/user/${userId}`)}
-        width={width}
-        borderRadius="50%"
+        size={size}
         src={src}
-        fallbackSrc="https://via.placeholder.com/20"
-        alt="avatar_image"
+        fallbackSrc="https://bit.ly/broken-link"
+        name="avatar_image"
         marginRight={isOnline ? undefined : '2'}
-      />
-      {/* css 수정 */}
-      {isOnline && (
-        <Box
-          width="5px"
-          height="5px"
-          backgroundColor="green.400"
-          borderRadius="50%"
-          position="relative"
-          top="10"
-          right="2"
-        />
-      )}
+      >
+        {isOnline && <AvatarBadge boxSize="1.25em" bg="green.500" />}
+      </Avatar>
     </>
   );
 };
