554ae675b0515884ef1bbbc8ad9ad9bfb97dc2c6
Jeong-Taeho <nabi218218@gmail.com>
2023-09-14 20:18:49 +0900

    refactor: document를 ref로 수정

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -3,17 +3,16 @@ import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { CreatePoster } from '../apis/poster';
 import PrimaryButton from './common/PrimaryButton';
+import { useNavigate } from 'react-router-dom';
 import {
   FormControl,
   FormLabel,
   Input,
   Text,
-  Container,
   Stack,
   Image,
   Flex,
   Spacer,
-  Button,
   Textarea,
   Center,
 } from '@chakra-ui/react';
@@ -36,18 +35,17 @@ export interface FileImage {
 
 const CreatePost = () => {
   const { register, handleSubmit, reset } = useForm<NewPostData>();
-
   const [selectedImages, setSelectedImages] = useState<FileImage[]>([]); // File 배열 형태로 선택한 파일들이 저장
-  const [loadedMap, setLoadedMap] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   const handlePrev = () => {
-    console.log('이전화면으로 이동하는 버튼으로 구현');
+    navigate('/');
   };
 
   const onSubmit = async (data: NewPostData) => {
     const formData = new FormData();
     formData.append('title', data.title);
-    formData.append('channelId', '64fbfdabe8468d1ed414e307');
+    formData.append('channelId', import.meta.env.VITE_MAIN_CHANNELID);
 
     if (selectedImages[0]) {
       formData.append('image', selectedImages[0].file);
@@ -58,7 +56,6 @@ const CreatePost = () => {
 
       setSelectedImages([]);
       reset();
-      console.log(response);
       return response;
     } catch (error) {
       console.error(error);
@@ -73,26 +70,26 @@ const CreatePost = () => {
     event
   ) => {
     if (event.target.files) {
-      // FileList 객체 반환
       const newFilesArray = Array.from(event.target.files).map((file) => ({
         id: uuidv4(),
         file,
-      })); // 지금 담은 이미지들
+      }));
       if (newFilesArray.length + selectedImages.length > 5) {
         alert('최대 5개의 이미지만 업로드 할 수 있습니다.');
         return;
       }
-      setSelectedImages([...selectedImages, ...newFilesArray]);
+      setSelectedImages((prev) => [...prev, ...newFilesArray]);
     }
-    event.target.value = ''; //똑같은 파일을 올려도 지장이 없도록
+    event.target.value = '';
   };
 
   const handleAddFile = () => {
-    (document.querySelector('input[type="file"]') as HTMLInputElement).click();
+    const inputRef = register('image').ref as unknown as HTMLInputElement;
+    inputRef?.click();
   };
 
   return (
-    <Container maxW="container.sm" mt="5">
+    <>
       <Flex>
         <ChevronLeftIcon boxSize={10} onClick={handlePrev} />
         <Spacer />
@@ -101,58 +98,54 @@ const CreatePost = () => {
         </Text>
         <Spacer />
       </Flex>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack spacing={4}>
-          <FormControl mt={5}>
-            <FormLabel>제목</FormLabel>
-            <Input
-              type="text"
-              placeholder="제목을 입력하세요."
-              {...register('title')}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>여행지</FormLabel>
-            <Button onClick={() => setLoadedMap(!loadedMap)}>
-              카카오맵 불러오기
-            </Button>
-          </FormControl>
-          <FormControl>
-            <FormLabel>후기</FormLabel>
-            <Textarea />
-          </FormControl>
-          <FormControl>
-            <Flex gap={5} overflow="hidden">
-              {selectedImages.length ? (
-                selectedImages.map(({ id, file }) => (
-                  <Flex key={id}>
-                    <Image boxSize="150px" src={URL.createObjectURL(file)} />
-                    <SmallCloseIcon onClick={() => handleRemoveImage(id)} />
-                  </Flex>
-                ))
-              ) : (
-                <Image src="https://via.placeholder.com/150" />
-              )}
-            </Flex>
-            <FormLabel>사진 (최대5개)</FormLabel>
-            <Input
-              type="file"
-              accept="image/*"
-              multiple
-              display="none"
-              {...register('image')}
-              onChange={handleFileSelect}
-            />
-            <PlusSquareIcon boxSize={8} onClick={handleAddFile} />
-          </FormControl>
-          <Center>
-            <PrimaryButton type="submit" w="200px">
-              글 올리기
-            </PrimaryButton>
-          </Center>
-        </Stack>
-      </form>
-    </Container>
+      <Stack as="form" onSubmit={handleSubmit(onSubmit)} spacing={4}>
+        <FormControl mt={5}>
+          <FormLabel>제목</FormLabel>
+          <Input
+            type="text"
+            placeholder="제목을 입력하세요."
+            {...register('title')}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>여행지</FormLabel>
+          <PrimaryButton w="180px">카카오맵 불러오기</PrimaryButton>
+        </FormControl>
+        <FormControl>
+          <FormLabel>후기</FormLabel>
+          <Textarea />
+        </FormControl>
+        <FormControl>
+          <Flex gap={5} overflow="hidden">
+            {selectedImages.length ? (
+              selectedImages.map(({ id, file }) => (
+                <Flex key={id}>
+                  <Image boxSize="150px" src={URL.createObjectURL(file)} />
+                  <SmallCloseIcon onClick={() => handleRemoveImage(id)} />
+                </Flex>
+              ))
+            ) : (
+              <Image src="https://via.placeholder.com/150" />
+            )}
+          </Flex>
+          <FormLabel>사진 (최대5개)</FormLabel>
+          <Input
+            type="file"
+            accept="image/*"
+            multiple
+            display="none"
+            {...register('image')}
+            onChange={handleFileSelect}
+          />
+          <PlusSquareIcon boxSize={8} onClick={handleAddFile} />
+        </FormControl>
+        <Center>
+          <PrimaryButton type="submit" w="200px">
+            글 올리기
+          </PrimaryButton>
+        </Center>
+      </Stack>
+    </>
   );
 };
 
