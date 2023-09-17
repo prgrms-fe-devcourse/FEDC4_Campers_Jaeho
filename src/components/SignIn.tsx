f71887dd4e71100eaf75b0c0ed4dbfe1185ab756
sukvvon <sukvvon@gmail.com>
2023-09-18 01:39:56 +0900

    feat: SignIn에 api 연결 후 예외 처리

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,14 +1,13 @@
-import { useForm } from 'react-hook-form';
 import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { ButtonGroup, Text } from '@chakra-ui/react';
 import PrimaryButton from './common/PrimaryButton';
 import AuthInputField from './Auth/AuthInputField';
+import { useNavigate } from 'react-router-dom';
+import { signin } from '../apis/auth';
+import { setLocalStorage } from '../utils/storage';
 import passwordValidation from '../utils/passwordValidation';
-
-type SignInFormValues = {
-  signInEmail: string;
-  signInPassword: string;
-};
+import { SignInValues as SignInFormValues } from '../types/auth';
 
 const SignIn = () => {
   const {
@@ -18,18 +17,32 @@ const SignIn = () => {
     formState: { errors },
     reset,
   } = useForm<SignInFormValues>();
+  const navigate = useNavigate();
 
-  const onSubmit = (data: SignInFormValues) => {
-    console.log(data);
+  const onSubmit = async (data: SignInFormValues) => {
+    const response = await signin(data);
 
-    reset();
+    if (typeof response === 'string') {
+      alert('이메일 혹은 비밀번호가 잘못되었습니다');
+      setFocus('email');
+      reset();
+
+      return;
+    }
+
+    if (typeof response === 'object') {
+      const { token } = response;
+
+      setLocalStorage('token', token);
+      navigate('/');
+    }
   };
 
   const registers = {
-    signInEmail: register('signInEmail', {
+    email: register('email', {
       required: '이메일은 필수 입력입니다.',
     }),
-    signInPassword: register('signInPassword', {
+    password: register('password', {
       required: '비밀번호 입력은 필수입니다.',
       minLength: {
         value: 8,
@@ -40,32 +53,32 @@ const SignIn = () => {
   };
 
   useEffect(() => {
-    setFocus('signInEmail');
+    setFocus('email');
   }, [setFocus]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <AuthInputField
-        {...registers.signInEmail}
-        error={errors.signInEmail}
-        id={'signin-email'}
-        label={'이메일'}
-        placeholder={'이메일을 입력해주세요'}
+        {...registers.email}
+        error={errors.email}
+        id="signin-email"
+        label="이메일"
+        placeholder="이메일을 입력해주세요"
       />
       <AuthInputField
-        {...registers.signInPassword}
-        error={errors.signInPassword}
-        id={'signin-password'}
-        label={'비밀번호'}
-        placeholder={'비밀번호를 입력해주세요'}
+        {...registers.password}
+        error={errors.password}
+        id="signin-password"
+        label="비밀번호"
+        placeholder="비밀번호를 입력해주세요"
         isPassword
       >
-        <Text fontSize={'xs'} color={'blackAlpha.600'}>
+        <Text fontSize="xs" color="blackAlpha.600">
           비밀번호는 8자 이상이면서 특수문자(!, @, #, $, %, ^, &, *, (, )), 영어
           대소문자, 숫자는 각각 최소 1개 이상 있어야합니다.
         </Text>
       </AuthInputField>
-      <ButtonGroup my={2} justifyContent={'center'} width="100%">
+      <ButtonGroup my={2} justifyContent="center" width="100%">
         <PrimaryButton type="submit">로그인</PrimaryButton>
         <PrimaryButton onClick={() => reset()}>초기화</PrimaryButton>
       </ButtonGroup>
