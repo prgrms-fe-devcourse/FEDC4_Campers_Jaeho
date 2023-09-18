9819b5f121f1e00e19a7003f5b122d22fde533b0
euan-lee <maplestory1419@naver.com>
2023-09-18 09:17:39 +0900

    feat: temperaturebar detail-page에 맞게 설정

---
+++ src/components/common/TemperatureBar.tsx
@@ -0,0 +1,31 @@
+import { Flex, Progress, Text, ProgressProps } from '@chakra-ui/react';
+
+type TemperatureBarProps = ProgressProps & {
+  value: number;
+  size?: string;
+};
+
+const TemperatureBar = ({
+  value,
+  size = 'xs',
+  ...props
+}: TemperatureBarProps) => {
+  return (
+    <Flex align="center" maxWidth={value}>
+      <Progress
+        {...props}
+        colorScheme="green"
+        size={size}
+        value={value}
+        width="100%"
+        borderRadius="4"
+        marginRight="1"
+      />
+      <Text color="green.400" fontSize="5">
+        {value}%
+      </Text>
+    </Flex>
+  );
+};
+
+export default TemperatureBar;
