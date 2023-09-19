095cf9069f42eb22d438125ebd57829b6fef97f6
Jeong-Taeho <nabi218218@gmail.com>
2023-09-19 15:23:59 +0900

    feat: 검색 페이지 구현

---
+++ src/pages/Search.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@chakra-ui/react';
+import SearchA from '../components/Search';
+
+const Search = () => {
+  return (
+    <Container mt={5}>
+      <SearchA />
+    </Container>
+  );
+};
+
+export default Search;
