1dd08a84f8a93db7c3bf6c736dc1779521991dcd
Jeong-Taeho <nabi218218@gmail.com>
2023-09-19 15:22:52 +0900

    feat: User 데이터 카드 컴포넌트

---
+++ src/components/Search/SearchResponse/UserCard.tsx
@@ -0,0 +1,21 @@
+import { UserResponse } from '../../../apis/types';
+import PrimaryAvatar from '../../common/PrimaryAvatar';
+import { Card, CardHeader, Flex } from '@chakra-ui/react';
+import UserInfoItem from '../../common/UserInfoItem';
+const UserCard = ({ userData }: { userData: UserResponse }) => {
+  const { image, _id, isOnline, fullName, email } = userData;
+  return (
+    <Card>
+      <CardHeader>
+        <Flex>
+          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+            <PrimaryAvatar src={image} userId={_id} isOnline={isOnline} />
+            <UserInfoItem title={fullName} subTitle={email} />
+          </Flex>
+        </Flex>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default UserCard;
