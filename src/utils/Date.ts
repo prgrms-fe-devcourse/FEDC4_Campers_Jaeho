6741d45f3f1813f352976a897afd14979165c71e
euan-lee <maplestory1419@naver.com>
2023-09-20 17:57:22 +0900

    fix: date.ts 파일 삭제

--- src/utils/Date.ts
+++
@@ -1,8 +0,0 @@
-export const formatDate = (inputString: string) => {
-  const date = new Date(inputString);
-  return date.toLocaleString(undefined, {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  });
-};
