2586705bb60c27022b5d5d17f743a3e273411bc0
euan-lee <maplestory1419@naver.com>
2023-09-20 09:00:01 +0900

    fix: comment axios함수 exception 처리

--- src/apis/comment.ts
+++ src/apis/comment.ts
@@ -1,23 +1,33 @@
 import { axiosInterface } from './axios';
+import { AxiosError } from 'axios';
 
 type CommentData = {
   comment: string;
   postId: string;
 };
+
 export const createComment = async ({ comment, postId }: CommentData) => {
   try {
     await axiosInterface.post('/comments/create', {
       comment,
       postId,
     });
+    alert('success');
   } catch (error) {
-    console.error(error);
+    if (error instanceof AxiosError) {
+      console.error(error.message);
+    } else if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error(error);
+    }
   }
 };
 
-export const deleteComment = async (postId: string) => {
+export const deleteComment = async (Id: string) => {
   try {
-    await axiosInterface.delete(`/comments/delete/${postId}`);
+    await axiosInterface.delete(`/comments/delete/${Id}`);
+    alert('success');
   } catch (error) {
     console.error(error);
   }
