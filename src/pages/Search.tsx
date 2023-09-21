146b0d3b6fd2d372e613b1f603c88bf12602c010
Jeong-Taeho <nabi218218@gmail.com>
2023-09-21 15:11:19 +0900

    refactor: 검색페이지 기능에 맞도록 재수정

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,79 @@
-import { Container } from '@chakra-ui/react';
-import SearchForm from '../components/Search';
+import { Container, Image, Text } from '@chakra-ui/react';
+import searchImage from '../assets/images/search.png';
+import PrimaryInfo from '../components/common/PrimaryInfo';
+import SearchBar from '../components/Search/SearchBar';
+import { useNavigate } from 'react-router-dom';
+import { useQueryPost } from '../hooks/useQueryPost';
+import { PostResponse } from '../types/post';
+import { User } from '../types/user';
+import { useParams } from 'react-router-dom';
+import UserCard from '../components/Search/UserCard';
+import PostCard from '../components/Search/PostCard';
+import NoResult from '../components/common/NoResult';
+import Loading from '../components/common/Loading';
 
 const Search = () => {
+  const navigate = useNavigate();
+  const { keyword } = useParams<{ keyword: string }>();
+  const {
+    getAllBoth: { data, isLoading, isError },
+  } = useQueryPost(keyword);
+
+  const userResult = data?.filter((item): item is User => 'fullName' in item);
+  const postResult = data?.filter(
+    (item): item is PostResponse => 'title' in item
+  );
+
+  isError && navigate('/search');
+
   return (
-    <Container mt={5}>
-      <SearchForm />
+    <Container mt={5} textAlign="center">
+      <SearchBar />
+      {keyword ? (
+        isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <Text fontSize="3xl" as="b">
+              "{keyword}" 검색 결과 {data?.length}개
+            </Text>
+            <Text fontSize="2xl">Users</Text>
+            {userResult?.length !== 0 ? (
+              userResult?.map((res) => (
+                <UserCard key={res._id} userData={res} />
+              ))
+            ) : (
+              <NoResult />
+            )}
+            <Text fontSize="2xl">Posts</Text>
+            {postResult?.length !== 0 ? (
+              postResult?.map((res) => (
+                <PostCard key={res._id} postData={res} />
+              ))
+            ) : (
+              <NoResult />
+            )}
+            {!userResult?.length && !postResult?.length && (
+              <Text
+                textAlign="center"
+                color="green.400"
+                as="b"
+                onClick={() => navigate('/createpost')}
+              >
+                이 곳에 대한 첫 글 써보기
+              </Text>
+            )}
+          </>
+        )
+      ) : (
+        <>
+          <Image src={searchImage} />
+          <PrimaryInfo
+            title="찾는 게 있으신가요?"
+            subTitle="키워드를 검색해보세요"
+          />
+        </>
+      )}
     </Container>
   );
 };
