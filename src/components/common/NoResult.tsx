96e17803477bb671548374f83dec83affa49cb65
nsr1349 <nsr1349@naver.com>
2023-09-22 04:15:17 +0900

    refactor: noResult 컴포넌트 서브타이틀 변경

--- src/components/common/NoResult.tsx
+++ src/components/common/NoResult.tsx
@@ -6,7 +6,7 @@ const NoResult = ({ src }: ImageProps) => {
       <Image src={src} />
       <PrimaryInfo
         title="검색 결과가 없습니다..."
-        subTitle="누구도 도달하지 못한 미지의 땅일지도 모르겠네요."
+        subTitle="검색어가 정확한지 확인해보세요!"
       />
     </>
   );
