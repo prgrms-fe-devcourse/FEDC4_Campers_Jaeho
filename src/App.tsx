f18b03353a272805a816f626775fe61a50ed5582
kim <perfumeplaylist.gmail.com>
2023-09-22 13:34:20 +0900

    fix: router 추가/로그아웃 되는 훅 주석 처리 / useFilter 리펙토링

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import UserProfile from './pages/UserProfile';
 import Detail from './pages/Detail';
 import Search from './pages/Search';
 import SearchAll from './components/Search/SearchResponse';
-import User from './pages/User';
+import UserList from './pages/UserList';
 
 const App = () => {
   return (
@@ -21,7 +21,7 @@ const App = () => {
         <Route path={ROUTES.CREATE_POST} element={<Create />} />
         <Route path={ROUTES.USER_EDIT} element={<></>} />
         <Route path={ROUTES.USER_PROFILE} element={<UserProfile />} />
-        <Route path={ROUTES.USER_LIST} element={<User />} />
+        <Route path={ROUTES.USER_LIST} element={<UserList />} />
         <Route path={ROUTES.CHAT_LIST} element={<></>} />
         <Route path={ROUTES.CHAT} element={<></>} />
         <Route path={ROUTES.DETAIL} element={<Detail />} />
