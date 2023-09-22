72d0a8dd69c0928fe58983de04013b9c291835ee
kim <perfumeplaylist.gmail.com>
2023-09-22 20:52:01 +0900

    fix: 퐅더명 수정된거 삭제

--- src/hooks/apis/Query/useLogout.tsx
+++
@@ -1,8 +0,0 @@
-import { logout } from '../../../apis/auth';
-import { useQuery } from '@tanstack/react-query';
-
-export const useLogout = () => {
-  const postLogout = useQuery(['logout'], logout);
-
-  return postLogout;
-};
