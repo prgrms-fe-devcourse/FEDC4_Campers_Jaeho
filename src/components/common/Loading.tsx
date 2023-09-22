622d353b0ced69340c5ec0a0b01a28f544a62aa4
nsr1349 <nsr1349@naver.com>
2023-09-22 15:59:46 +0900

    fix: props 아래로 이동

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -3,12 +3,12 @@ import { Spinner, SpinnerProps } from '@chakra-ui/react';
 const Loading = ({ ...props }: SpinnerProps) => {
   return (
     <Spinner
-      {...props}
       thickness="4px"
       speed="0.65s"
       emptyColor="gray.200"
       color="blue.500"
       size="xl"
+      {...props}
     />
   );
 };
