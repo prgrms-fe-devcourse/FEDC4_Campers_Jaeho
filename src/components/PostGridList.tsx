384bca760aed09f009abc52317bfb61457dd7ef5
nsr1349 <nsr1349@naver.com>
2023-09-23 15:49:33 +0900

    refactor: 마지막으로 최종수정. hover 추가 및 개선

--- src/components/PostGridList.tsx
+++ src/components/PostGridList.tsx
@@ -31,50 +31,45 @@ const PostGridList = ({
           borderRadius="10"
           bgImg={image || '../src/assets/images/no_image.png'}
           bgRepeat="no-repeat"
-          bgSize="cover"
+          bgSize="100% auto;"
           bgPosition="center"
-          pos="relative"
+          transition="all 0.3s"
+          _hover={{ bgSize: '110% auto;' }}
           {...props}
         >
-          <>
+          <PrimaryLink router={`${ROUTES.DETAIL.replace(':postId', _id)}`}>
             {isShowText && (
               <Box
                 pos="relative"
                 w="100%"
                 h="100%"
                 bg="linear-gradient(
-              180deg,
-              rgba(2, 0, 36, 0) 0%,
-              rgba(33, 33, 116, 0) 20%,
-              rgba(0, 0, 0, 1) 100%
-            )"
+                  180deg,
+                  rgba(2, 0, 36, 0) 0%,
+                  rgba(33, 33, 116, 0) 20%,
+                  rgba(0, 0, 0, 0.6) 100%
+                )"
               >
-                <Text pos="absolute" bottom="50" left="15" color="#919392">
+                <Text pos="absolute" bottom="50" left="15" color="#D3DCDE">
                   북한
                 </Text>
                 <Text
                   pos="absolute"
                   bottom="15"
                   left="15"
-                  color="white"
+                  color="#eee"
                   fontSize="xl"
                   overflow="hidden"
                   maxW="90%"
                   whiteSpace="nowrap"
                   textOverflow="ellipsis"
+                  fontWeight="bold"
                 >
                   {title}
                 </Text>
               </Box>
             )}
-            <PrimaryLink
-              pos="absolute"
-              w="100%"
-              h="100%"
-              top={0}
-              router={`${ROUTES.DETAIL.replace(':postId', _id)}`}
-            ></PrimaryLink>
-          </>
+          </PrimaryLink>
         </AspectRatio>
       ))}
     </SimpleGrid>
