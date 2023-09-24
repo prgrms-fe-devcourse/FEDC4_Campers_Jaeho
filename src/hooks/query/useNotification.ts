6a843edd8916fb08b7115540fa1321e341233cb5
nsr1349 <nsr1349@naver.com>
2023-09-24 17:10:06 +0900

    fix: 병합과정에서 충돌 해결, 이어서 수정 예정

---
+++ src/hooks/query/useNotification.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import { getNotification } from '../../apis/Notification';
+
+export const useNotification = () => {
+  const getNewNotification = useQuery(
+    ['new', 'notification'],
+    () => getNotification
+  );
+
+  return getNewNotification;
+};
