6e541632a00f0b61e05d61b528dfd99bf93c8361
Jeong-Taeho <nabi218218@gmail.com>
2023-09-25 16:35:27 +0900

    feat: 이미지 없을 때와 있을 때 사용할 컴포넌트

---
+++ src/components/common/PrimaryImage.tsx
@@ -0,0 +1,14 @@
+import { Image, ImageProps } from '@chakra-ui/react';
+const PrimaryImage = ({
+  imageSrc,
+  ...props
+}: ImageProps & { imageSrc: string }) => {
+  return (
+    <Image
+      {...props}
+      src={imageSrc ? imageSrc : 'https://via.placeholder.com/150'}
+    />
+  );
+};
+
+export default PrimaryImage;
