804fec8ba4799ee5652bf0ac897570da4867b6f8
sukvvon <sukvvon@gmail.com>
2023-09-26 07:15:57 +0900

    feat: Auth에 이전 링크 추가

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -2,7 +2,10 @@ import { Container } from '@chakra-ui/react';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
 import PrimaryTabsSet from '../components/common/PrimaryTabsSet';
+import PrimaryHeader from '../components/common/PrimaryHeader';
+import PrimaryLink from '../components/common/PrimaryLink';
 import useAuthCheck from '../hooks/useAuthCheck';
+import { GrFormPrevious } from 'react-icons/gr';
 
 const Auth = () => {
   useAuthCheck();
@@ -14,6 +17,11 @@ const Auth = () => {
       maxW="container.sm"
       justifyContent="center"
     >
+      <PrimaryHeader w="100%" maxW="container.sm" pos="fixed" top={0} p={6}>
+        <PrimaryLink cursor="pointer" router={-1}>
+          <GrFormPrevious fontSize="25px" />
+        </PrimaryLink>
+      </PrimaryHeader>
       <PrimaryTabsSet
         tabTexts={['로그인', '회원가입']}
         tabPanelChildrens={[<SignIn />, <SignUp />]}
