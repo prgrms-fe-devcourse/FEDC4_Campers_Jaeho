414b845782697ea8a94ed5435a541d41d42c8cca
euan-lee <maplestory1419@naver.com>
2023-09-25 18:00:52 +0900

    feat: like optimistic update

--- src/hooks/mutation/useRecommend.ts
+++ src/hooks/mutation/useRecommend.ts
@@ -1,10 +1,13 @@
 import { useMutation } from '@tanstack/react-query';
 import { CreateRecommend } from '../../apis/social';
-
+import { DeleteRecommend } from '../../apis/social';
 export const useRecommend = () => {
-  const postRecommend = useMutation((postId: string) =>
+  const createRecommend = useMutation((postId: string) =>
     CreateRecommend(postId)
   );
+  const deleteRecommend = useMutation((postId: string) =>
+    DeleteRecommend(postId)
+  );
 
-  return postRecommend;
+  return { createRecommend, deleteRecommend };
 };
