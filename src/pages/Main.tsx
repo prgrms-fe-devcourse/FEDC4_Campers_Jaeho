4d6903c1de9fdc5f7e9f43961b2f60b85780ed23
sukvvon <sukvvon@gmail.com>
2023-09-25 16:53:10 +0900

    feat: Main에 Container 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,16 +1,17 @@
 import { SearchIcon } from '@chakra-ui/icons';
-import { Text } from '@chakra-ui/react';
+import { Container, Text } from '@chakra-ui/react';
 import NavigationBar from '../components/NavigationBar';
 import { ROUTES } from '../constants/routes';
 import PrimaryHeader from '../components/common/PrimaryHeader';
 import PrimaryLink from '../components/common/PrimaryLink';
 import MainPagePosts from '../components/Main/MainPagePosts';
 import CircleIconBg from '../components/common/CircleIconBg';
-function Main() {
+
+const Main = () => {
   return (
-    <>
-      <PrimaryHeader h="60px" p="0 15px">
-        <Text fontSize="24px" flexGrow="1">
+    <Container h="100vh" maxW="container.sm" justifyContent="center" p={0}>
+      <PrimaryHeader h="60px">
+        <Text fontSize="24px" flexGrow={1}>
           Campers
         </Text>
         <PrimaryLink router={ROUTES.SEARCH}>
@@ -21,8 +22,8 @@ function Main() {
       </PrimaryHeader>
       <MainPagePosts />
       <NavigationBar />
-    </>
+    </Container>
   );
-}
+};
 
 export default Main;
