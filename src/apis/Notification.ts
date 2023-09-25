80f3336b55151c4f9d0ba7aa76970d95da7e620c
Jeong-Taeho <nabi218218@gmail.com>
2023-09-25 16:34:00 +0900

    refactor: 알람 타입 지정

--- src/apis/Notification.ts
+++ src/apis/Notification.ts
@@ -1,9 +1,12 @@
+import { NotificationResponse } from './../types/user';
 import instance from './axios';
 import { AxiosError } from 'axios';
 
 export const getNotification = async () => {
   try {
-    const { data } = await instance.get('/notifications');
+    const { data } = await instance.get<NotificationResponse[] | null>(
+      '/notifications'
+    );
 
     return data;
   } catch (error) {
