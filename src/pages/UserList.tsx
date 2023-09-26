f80478bc69a6fba75563d04e573c582c9441ea42
nsr1349 <nsr1349@naver.com>
2023-09-26 21:50:08 +0900

    refactor: userList 수정

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -1,97 +1,20 @@
-import { List, Spacer, Text, VStack } from '@chakra-ui/react';
-import NavigationBar from '../components/NavigationBar';
-import PrimaryLink from '../components/common/PrimaryLink';
-import PrimaryHeader from '../components/common/PrimaryHeader';
-import PrimaryList from '../components/common/PrimaryList';
-import PrimaryText from '../components/common/PrimaryText';
-import PrimaryTabsSet from '../components/common/PrimaryTabsSet';
-import { IoArrowBackOutline } from 'react-icons/io5';
-import { ROUTES } from '../constants/routes';
+import { Stack } from '@chakra-ui/react';
 import { useFilter } from '../hooks/useFilter';
-import { User } from '../types/user';
-import PrimaryContainer from '../components/common/PrimaryContainer';
+import UserCard from '../components/Search/UserCard';
 
 const UserList = () => {
-  const [filteredData, switchData] = useFilter([]);
+  const { filteredData } = useFilter();
 
   return (
-    <PrimaryContainer>
-      <VStack spacing={3} align="stretch">
-        <PrimaryHeader>
-          <PrimaryLink
-            router={ROUTES.MAIN}
-            color="#0D1321"
-            width="5%"
-            as="button"
-            mr={1}
-          >
-            <IoArrowBackOutline fontSize={22} />
-          </PrimaryLink>
-          <Spacer />
-          <Text fontSize={20}>Chat</Text>
-          <Spacer />
-        </PrimaryHeader>
-        <PrimaryTabsSet
-          handleTabsChangeAdditionalFn={(index: number) => {
-            if (typeof switchData === 'function') {
-              switchData(index);
-            }
-          }}
-          tabTexts={['total', 'follwer', 'following']}
-          tabPanelChildrens={[
-            Array.isArray(filteredData) && (
-              <List spacing={3}>
-                {filteredData.map(
-                  ({ _id, fullName, isOnline, image }: Partial<User>) => (
-                    <PrimaryList
-                      src={image}
-                      userId={_id!}
-                      isOnline={isOnline!}
-                      key={_id}
-                    >
-                      <PrimaryText>{fullName}</PrimaryText>
-                    </PrimaryList>
-                  )
-                )}
-              </List>
-            ),
-            Array.isArray(filteredData) && (
-              <List spacing={3}>
-                {filteredData.map(
-                  ({ _id, fullName, isOnline, image }: Partial<User>) => (
-                    <PrimaryList
-                      src={image}
-                      userId={_id!}
-                      isOnline={isOnline!}
-                      key={_id}
-                    >
-                      <PrimaryText>{fullName}</PrimaryText>
-                    </PrimaryList>
-                  )
-                )}
-              </List>
-            ),
-            Array.isArray(filteredData) && (
-              <List spacing={3}>
-                {filteredData.map(
-                  ({ _id, fullName, isOnline, image }: Partial<User>) => (
-                    <PrimaryList
-                      src={image}
-                      userId={_id!}
-                      isOnline={isOnline!}
-                      key={_id}
-                    >
-                      <PrimaryText>{fullName}</PrimaryText>
-                    </PrimaryList>
-                  )
-                )}
-              </List>
-            ),
-          ]}
-        />
-        <NavigationBar />
-      </VStack>
-    </PrimaryContainer>
+    <Stack mt="20px">
+      {filteredData && (
+        <>
+          {filteredData.map((userData) => (
+            <UserCard userData={userData} key={userData._id} />
+          ))}
+        </>
+      )}
+    </Stack>
   );
 };
 
