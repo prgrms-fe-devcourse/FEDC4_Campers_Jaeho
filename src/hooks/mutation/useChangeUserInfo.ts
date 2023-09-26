727a78cec53767097eed8856d15d199f7afe35ff
Jeong-Taeho <nabi218218@gmail.com>
2023-09-26 15:52:03 +0900

    refactor: 이미지 변경 반환타입 명시

--- src/hooks/mutation/useChangeUserInfo.ts
+++ src/hooks/mutation/useChangeUserInfo.ts
@@ -1,10 +1,11 @@
+import { User } from './../../types/user';
 import { ChangeProfileImage, ChangeUserInfo, UserInfo } from '../../apis/user';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const useChangeUserInfo = () => {
   const queryClient = useQueryClient();
 
-  const postProfileImage = useMutation<unknown, unknown, FormData>(
+  const postProfileImage = useMutation<User | undefined, unknown, FormData>(
     (formData) => ChangeProfileImage(formData!),
     {
       onSuccess(data) {
