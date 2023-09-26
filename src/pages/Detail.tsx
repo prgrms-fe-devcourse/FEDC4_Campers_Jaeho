e58935dfcd74f66e8a414b8c34c927d2cb710338
euan-lee <maplestory1419@naver.com>
2023-09-26 22:59:40 +0900

    fix: 댓글함수 낙관적 업데이트 변수명  수정

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -22,44 +22,21 @@ import PrimaryButton from '../components/common/PrimaryButton';
 import RecommendButton from '../components/common/RecommendButton';
 import { formatDate } from '../utils/formateData';
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
 import PrimaryText from '../components/common/PrimaryText';
 import { useDetailPost } from '../hooks/query/useDetailPost';
 import { InputForm } from '../components/common/InputForm';
-import { CommentInfo } from '../types/detail';
-import { v4 as uuidv4 } from 'uuid';
 import { useUserInfoContext } from '../contexts/UserInfoProvider';
+
 const Detail = () => {
   const { postId } = useParams<{ postId: string }>();
   const [isDrawerOpen, setIsDrawerOpen] = useBoolean();
-  const [comments, setComments] = useState<CommentInfo[]>([]);
   const { data: { postInfo, commentInfo, likeInfo } = {}, isLoading } =
     useDetailPost(postId);
-  const userInfo = useUserInfoContext();
-  const doesUserIdExist = (arr, id) => {
+  const { userInfo } = useUserInfoContext();
+  const doesUserIdExist = (arr: { user: string }[], id: string): boolean => {
     const foundUser = arr.find((item) => item.user === id);
 
     return !!foundUser;
-  };
-  const handleComment = (newcomment: string): void => {
-    const Info = {
-      _id: uuidv4(),
-      fullName: userInfo?.fullName,
-      isOnline: userInfo?.isOnline,
-      image: userInfo?.image,
-    };
-    setComments([...comments, { comment: newcomment, ...Info }]);
-  };
-
-  const handleDelete = (commentId) => {
-    console.log(commentId);
-    if (commentId === userInfo?._id) {
-      alert('deldete');
-
-      deleteComment.mutateAsync({
-        postId: userInfo?._id,
-      });
-    }
   };
 
   return (
@@ -73,17 +50,15 @@ const Detail = () => {
             <Flex justifyContent="space-between">
               <Box>
                 <Stack spacing={2}>
-                  <PrimaryText
-                    fontSize={15}
-                    children={formatDate(postInfo?.updatedAt)}
-                  />
+                  <PrimaryText fontSize={15}>
+                    {postInfo?.updatedAt ? formatDate(postInfo.updatedAt) : ''}
+                  </PrimaryText>
                   <WrapItem>
                     <PrimaryAvatar
-                      userId={postInfo?._id}
                       size={'sm'}
                       name={postInfo?.fullName}
                       src={postInfo?.authorImage}
-                      isOnline={postInfo?.isOnline}
+                      isOnline={postInfo?.isOnline || false}
                     />
                     <Box>
                       <PrimaryText
@@ -96,16 +71,22 @@ const Detail = () => {
                 </Stack>
               </Box>
               <Box>
-                <RecommendButton
-                  recommendCount={likeInfo?.length}
-                  isRecommended={doesUserIdExist(likeInfo, userInfo?._id)}
-                  bg="#D3DCDE"
-                  width={20}
-                  height={30}
-                  top={10}
-                  size={'lg'}
-                  postId={postId}
-                />
+                {postId && likeInfo && (
+                  <RecommendButton
+                    recommendCount={likeInfo?.length ?? null}
+                    isRecommended={
+                      userInfo?._id !== null && userInfo?._id !== undefined
+                        ? doesUserIdExist(likeInfo, userInfo._id)
+                        : false
+                    }
+                    bg="#D3DCDE"
+                    width={20}
+                    height={30}
+                    top={10}
+                    size={'lg'}
+                    postId={postId}
+                  />
+                )}
               </Box>
             </Flex>
           </Box>
@@ -126,11 +107,10 @@ const Detail = () => {
                 .map((comment) => (
                   <Comment
                     comment={comment.comment}
-                    image={comment.image}
-                    isOnline={comment.isOnline}
+                    image={comment.author_image}
+                    isOnline={comment?.isOnline || false}
                     name={comment.fullName}
-                    userId={comment._id}
-                    handleDelete={handleDelete}
+                    userId={comment.author_id}
                   />
                 ))}
             </Box>
@@ -159,11 +139,10 @@ const Detail = () => {
                     {commentInfo?.map((comment) => (
                       <Comment
                         comment={comment.comment}
-                        image={comment.image}
-                        isOnline={comment.isOnline}
+                        image={comment.author_image}
+                        isOnline={comment?.isOnline || false}
                         name={comment.fullName}
-                        userId={comment._id}
-                        handleDelete={handleDelete}
+                        userId={comment.author_id}
                       />
                     ))}
                   </DrawerBody>
@@ -172,7 +151,7 @@ const Detail = () => {
             </Center>
           </Box>
           <Box bg="#ECE9E9" maxW="100%">
-            <InputForm postId={postId} handleComment={handleComment} />
+            {postId && <InputForm postId={postId} />}
           </Box>
         </>
       )}
