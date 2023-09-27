cfef6dd90c1fe424f240775c539cab19586fe919
nsr1349 <nsr1349@naver.com>
2023-09-27 16:33:56 +0900

    refactor: weather loading 수정

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,18 +1,22 @@
 import { useLocation } from '../hooks/useLocation';
 import { useWeather } from '../hooks/query/useWeather';
-import { Flex, Box, Text, Stack } from '@chakra-ui/react';
+import { Flex, Box, Text, Stack, Center } from '@chakra-ui/react';
 import {
   FilterWeatherText,
   handleImageKeyword,
 } from '../utils/FilterWeatherText';
 import { GrLocation } from 'react-icons/gr';
 import PrimaryImage from './common/PrimaryImage';
+import Loading from './common/Loading';
 
 const Weather = () => {
   const location = useLocation();
   const { data, isLoading, isError } = useWeather(location.lat, location.lon);
   console.log(location);
-  if (isLoading) return <Box>Loading...</Box>;
+  // if (isLoading)
+  //   return (
+
+  //   );
 
   if (isError) return <Box>Error...</Box>;
 
@@ -24,37 +28,46 @@ const Weather = () => {
         borderRadius="10"
         px="20px"
         mx="10px"
-        h="150px"
+        maxH="150px"
       >
-        <Stack flex={1} pos="relative">
-          <Flex
-            align="center"
-            gap="5px"
-            p="3px 7px"
-            bgColor="white"
-            fontSize="12px"
-            flex={0}
-            pos="absolute"
-            borderRadius="20px"
-            top="-30px"
-          >
-            <GrLocation />
+        {isLoading ? (
+          <Center p="20px" w="100%">
+            <Loading size="md" />
+          </Center>
+        ) : (
+          <>
+            {' '}
+            <Stack flex={1} pos="relative" justify="center">
+              <Flex
+                align="center"
+                gap="5px"
+                p="3px 7px"
+                bgColor="white"
+                fontSize="12px"
+                flex={0}
+                pos="absolute"
+                borderRadius="20px"
+                top="-30px"
+              >
+                <GrLocation />
+                <Box>
+                  <Text> {data.name}</Text>
+                </Box>
+              </Flex>
+              <Text fontWeight="bold" fontSize="24px">
+                오늘은 {FilterWeatherText(data.weatherInfo.keyword)}
+              </Text>
+              <Text pos="absolute" bottom="-25px" fontSize="14px">
+                우산 챙겨가세요~
+              </Text>
+            </Stack>
             <Box>
-              <Text> {data.name}</Text>
+              <PrimaryImage
+                imageSrc={handleImageKeyword(data.weatherInfo.keyword)}
+              />
             </Box>
-          </Flex>
-          <Text fontWeight="bold" fontSize="26px">
-            오늘은 {FilterWeatherText(data.weatherInfo.keyword)}
-          </Text>
-          <Text pos="absolute" bottom="-25px" fontSize="14px">
-            우산 챙겨가세요~
-          </Text>
-        </Stack>
-        <Box>
-          <PrimaryImage
-            imageSrc={handleImageKeyword(data.weatherInfo.keyword)}
-          />
-        </Box>
+          </>
+        )}
       </Flex>
     </>
   );
