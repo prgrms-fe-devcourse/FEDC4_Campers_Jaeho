af2595a5958d6ef2c93ae08ddca6b7a2b4b60cd3
nsr1349 <nsr1349@naver.com>
2023-09-27 15:21:34 +0900

    refactor: weather css 바꿈

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from '../hooks/useLocation';
 import { useWeather } from '../hooks/query/useWeather';
-import { Flex, Box, Text } from '@chakra-ui/react';
+import { Flex, Box, Text, Stack } from '@chakra-ui/react';
 import {
   FilterWeatherText,
   handleImageKeyword,
@@ -11,22 +11,46 @@ import PrimaryImage from './common/PrimaryImage';
 const Weather = () => {
   const location = useLocation();
   const { data, isLoading, isError } = useWeather(location.lat, location.lon);
-
+  console.log(location);
   if (isLoading) return <Box>Loading...</Box>;
 
   if (isError) return <Box>Error...</Box>;
 
   return (
     <>
-      <Flex align="center" background="gray.300" borderRadius="10" px="3">
-        <Box flex={1}>
-          <Flex align="center" borderRadius="5" background="white">
-            <GrLocation style={{ width: '15px' }} />
-            <Text as="b">{data.name}</Text>
+      <Flex
+        align="center"
+        background="gray.300"
+        borderRadius="10"
+        px="20px"
+        mx="10px"
+        h="150px"
+      >
+        <Stack flex={1} pos="relative">
+          <Flex
+            align="center"
+            gap="5px"
+            p="3px 7px"
+            bgColor="white"
+            fontSize="12px"
+            flex={0}
+            pos="absolute"
+            borderRadius="20px"
+            top="-30px"
+          >
+            <GrLocation />
+            <Box>
+              <Text> {data.name}</Text>
+            </Box>
           </Flex>
-          <Text>오늘은 {FilterWeatherText(data.weatherInfo.keyword)}</Text>
-        </Box>
-        <Box flex="1">
+          <Text fontWeight="bold" fontSize="26px">
+            오늘은 {FilterWeatherText(data.weatherInfo.keyword)}
+          </Text>
+          <Text pos="absolute" bottom="-25px" fontSize="14px">
+            우산 챙겨가세요~
+          </Text>
+        </Stack>
+        <Box>
           <PrimaryImage
             imageSrc={handleImageKeyword(data.weatherInfo.keyword)}
           />
