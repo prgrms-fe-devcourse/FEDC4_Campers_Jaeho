8a4f79a265839a28d93fe975cf8869c7078f273e
nsr1349 <nsr1349@naver.com>
2023-10-03 14:27:18 +0900

    refactor: 네비게이션 바 변경, 컬러 고정

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,84 +1,72 @@
-import { Flex, Center, useColorModeValue } from '@chakra-ui/react';
+import { Flex, Center } from '@chakra-ui/react';
 import { AiFillHome } from 'react-icons/ai';
 import { IoIosAddCircle } from 'react-icons/io';
 import { ROUTES } from '../constants/routes';
 import PrimaryAvatar from './common/PrimaryAvatar';
 import PrimaryLink from './common/PrimaryLink';
 import { useUserInfoContext } from '../contexts/UserInfoProvider';
+import PrimaryButton from './common/PrimaryButton';
 
 const NavigationBar = () => {
   const { userInfo } = useUserInfoContext();
-  const iconColor = useColorModeValue('white', '#2D3748');
-  const boxColor = useColorModeValue('#2F3C4C', '#F0F0F0');
+  const navMenu = [
+    {
+      icon: <AiFillHome />,
+      link: ROUTES.MAIN,
+    },
+    {
+      icon: <IoIosAddCircle />,
+      link: ROUTES.CREATE_POST,
+    },
+    {
+      icon: (
+        <PrimaryAvatar
+          size="sm"
+          src={userInfo?.image ?? 'https://bit.ly/dan-abramov'}
+          isOnline={true}
+        />
+      ),
+      link: `${ROUTES.USER_INFO(userInfo?._id || '')}`,
+    },
+  ];
 
   return (
-    <Flex
-      w="100%"
-      h="50px"
-      cursor="pointer"
-      bgColor={boxColor}
-      align="center"
-      justify="center"
-      pos="fixed"
-      bottom={0}
-      maxW="60ch"
-    >
+    <>
       {userInfo ? (
-        <>
-          <PrimaryLink
-            router={ROUTES.MAIN}
-            flex={1}
-            h="100%"
-            transition="all 0.3s"
-            _hover={{ bgColor: 'gray.200' }}
-          >
-            <Center h="100%">
-              <AiFillHome fontSize={22} color={iconColor} />
-            </Center>
-          </PrimaryLink>
-          <PrimaryLink
-            router={ROUTES.CREATE_POST}
-            flex={1}
-            h="100%"
-            transition="all 0.3s"
-            _hover={{ bgColor: 'gray.200' }}
-          >
-            <Center h="100%">
-              <IoIosAddCircle fontSize={22} color={iconColor} />
-            </Center>
-          </PrimaryLink>
-          <PrimaryLink
-            flex={1}
-            color="#0D1321"
-            router={`${ROUTES.USER_INFO(userInfo?._id || '')}`}
-            h="100%"
-            transition="all 0.3s"
-            _hover={{ bgColor: 'gray.200' }}
-          >
-            <Center h="100%">
-              <PrimaryAvatar
-                size="sm"
-                src={userInfo.image ?? 'https://bit.ly/dan-abramov'}
-                isOnline={true}
-              />
-            </Center>
-          </PrimaryLink>
-        </>
-      ) : (
-        <PrimaryLink
+        <Flex
           w="100%"
-          h="100%"
-          bgColor="#28B67E"
-          color="white"
-          fontWeight="bold"
-          router={ROUTES.AUTH}
+          h="60px"
+          maxW="60ch"
+          pos="fixed"
+          bottom="0"
+          bgColor="#E6E9F2"
+          borderRadius="15px 15px 0 0 "
+          boxShadow="md"
+          overflow="hidden"
         >
-          <Center w="100%" h="100%">
-            로그인!
-          </Center>
-        </PrimaryLink>
+          <Flex w="100%" fontSize="28px" color="black">
+            {navMenu &&
+              navMenu.map(({ icon, link }) => (
+                <PrimaryLink
+                  router={link}
+                  flex={1}
+                  transition="all 0.3s"
+                  _hover={{ bgColor: '#D6DBEA' }}
+                  key={link}
+                >
+                  <Center h="100%">{icon}</Center>
+                </PrimaryLink>
+              ))}
+          </Flex>
+        </Flex>
+      ) : (
+        <Flex w="100%" maxW="60ch" pos="fixed" justify="center" bottom="0">
+          <PrimaryLink router={ROUTES.AUTH} w="95%">
+            <PrimaryButton w="100%">로그인!</PrimaryButton>
+          </PrimaryLink>
+        </Flex>
       )}
-    </Flex>
+    </>
   );
 };
 
