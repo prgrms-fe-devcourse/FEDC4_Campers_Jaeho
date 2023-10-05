44c164581d3b63c128685e1faf220481252954ba
sukvvon <sukvvon@gmail.com>
2023-10-05 21:38:34 +0900

    feat: '줄이기' 추가, 변수명 변경

--- src/components/Post/MoreText.tsx
+++ src/components/Post/MoreText.tsx
@@ -1,32 +1,31 @@
-import { Collapse, CollapseProps, Button, useBoolean } from '@chakra-ui/react';
-import { useEffect, useRef } from 'react';
+import { Collapse, Button, useBoolean } from '@chakra-ui/react';
+import { ComponentProps, useEffect, useRef } from 'react';
 
-const MoreText = ({ children, ...props }: CollapseProps) => {
+const MoreText = ({ children, ...props }: ComponentProps<typeof Collapse>) => {
+  const [isExtanded, setIsExtanded] = useBoolean();
+  const [isExtandNeed, setIsExtandNeed] = useBoolean();
   const ref = useRef<HTMLDivElement | null>(null);
-  const [isToggled, setIsToggled] = useBoolean();
-  const [isNeedToggle, setIsNeedToggle] = useBoolean(true);
-  const handleToggle = () => {
-    setIsToggled.toggle();
-  };
+
   useEffect(() => {
     if (ref.current) {
-      const firstChild = ref.current.firstChild as HTMLElement;
-      if (firstChild) {
-        const parentHeight = ref.current.offsetHeight;
-        const childHeight = firstChild.offsetHeight;
-        if (parentHeight >= childHeight) setIsNeedToggle.off();
+      const firstChildElement = ref.current.firstChild as HTMLElement;
+
+      if (firstChildElement) {
+        const parentElementHeight = ref.current.offsetHeight;
+        const childElementHeight = firstChildElement.offsetHeight;
+        if (parentElementHeight < childElementHeight) setIsExtandNeed.on();
       }
     }
-  }, [children]);
+  }, []);
 
   return (
     <>
-      <Collapse in={isToggled} ref={ref} {...props}>
+      <Collapse in={isExtanded} ref={ref} {...props}>
         {children}
       </Collapse>
-      {isNeedToggle && (
-        <Button size="sm" onClick={handleToggle} mt={4}>
-          {isToggled}더보기
+      {isExtandNeed && (
+        <Button size="sm" onClick={setIsExtanded.toggle} mt={4}>
+          {isExtanded ? '줄이기' : '더보기'}
         </Button>
       )}
     </>
